b6ea913684abc7d9a6326d2f77b6ca1b78757679
ccharry <chriseth@gmail.com>
2019-07-22 00:25:05 -0500

    loans and purchases being loaded correctly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Table, Button, Row, Col,
+  Card, CardBody,
+  CardTitle, ListGroup, ListGroupItem} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    purchases:[],
+    loans:[],
+    currentLoan:0
+  }
+
+  curr_formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0
+  })
+
+ 
+  componentWillMount(){
+
+    axios.get('http://localhost:5000/loan').then((response) => {
+      this.setState({
+        loans: response.data.loans
+      })
+    })
+
+
+  }
+  
+
+  render() {
+    
+    let purchases = this.state.purchases.map((purchase) => {
+      return (
+        <tr key={purchase.id}>
+          <td>{purchase.investor_name}</td>
+          <td>{purchase.amount}</td>
+          <td>{((purchase.amount/this.state.currentLoan.total)*100).toFixed(1)}</td>
+          <td>
+            <Button color="success" size="sm" className="mr-2">Edit</Button>{' '}
+            <Button color="danger" size="sm" className="mr-2">Delete</Button>{' '}
+          </td>
+        </tr>
+      )
+    });
+
+    let loans = this.state.loans.map((loan) => {
+      return (
+        <ListGroupItem key={loan.id} onClick={this.load_purchases.bind(this, loan.id)}>
+          <div className="clearfix">
+            <div className="float-left">
+              <Row>Product ID</Row>
+              <Row>{loan.product_id}</Row>
+            </div>
+            <div className="float-right">
+              <Row className="text-right">Advance</Row>
+              <Row className="text-right">{loan.advance_date.substring(0,10)}</Row>
+            </div>
+          </div>
+          <Row>
+            <Col className="text-right">
+              {this.curr_formatter.format(loan.total)}
+            </Col>
+          </Row>
+        </ListGroupItem>
+      )
+    });
+
+    return (
+      <div className="App container">
+        <Row>
+          <Col></Col>
+          <Col>APPLICATION</Col>
+          <Col>SEARCH</Col>
+          <Col>QA</Col>
+          <Col>DASHBOARD</Col>
+          <Col>USER ADMIN</Col>
+          <Col>ADMIN</Col>
+        </Row>
+        <Row>
+          <Col md="4" lg="4">
+          <Card>
+            <CardBody>
+              <CardTitle>Select a product to syndicate</CardTitle>
+              
+              <ListGroup>
+                {loans}
+              </ListGroup>
+              <div className="text-center" style={{ padding: '.5rem' }}>
+                <button className="btn btn-info mr-2">Close</button>
+                <button className="btn btn-info mr-2">Sell all</button>
+              </div>
+            </CardBody>
+          </Card>
+          </Col>
+          <Col md="8" lg="8">
+            Product ID {this.state.currentLoan.product_id}
+          <Table>
+          <thead>
+            <tr>
+              <th>Investor name</th>
+              <th>Sold</th>
+              <th>% Purchased</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {purchases}
+          </tbody>
+        </Table>
+          </Col>
+        </Row>
+        <Row >
+          <Col className="text-center small">&copy; LendingFront 2016</Col>
+        </Row>
+      </div>
+    );  
+  }
+
+  load_purchases(loan_id){
+    axios.post('http://localhost:5000/purchase/'+loan_id).then((response) => {
+      this.setState({
+        purchases: response.data.purchases
+      })
+    })
+
+    axios.get('http://localhost:5000/loan/'+loan_id).then((response) => {
+      this.setState({
+        currentLoan: response.data.loan
+      })
+    })
+  }
 }
 
 export default App;
